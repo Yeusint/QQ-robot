e6e912accc176391d57ee08e7027d3bd7953c510
Yeusint <hxx770308@163.com>
2022-03-06 11:45:47 +0800

    new

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ text = rev_msg()
 while True:
     text = rev_msg()
     print(text)
+    if text['message_type'] == 'group' and text['message'] == 'Yeusint':
+        send_msg({
+            'type': "group",
+            'id': str(text['group_id']),
+            'msg': '滴滴滴！！！'
+        })
