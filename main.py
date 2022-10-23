70b3a3de58cb9b44348d392fe78a03a8d492dc2f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 16:30:52 +0800

    change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graia.ariadne.entry import Ariadne, config, WebsocketClientConfig
 from graia.saya import Saya
 from creart import create
-App = Ariadne(config(2769124385, "770308", WebsocketClientConfig("ws://1.117.108.106:80")))
+App = Ariadne(config(2769124385, "770308", WebsocketClientConfig("ws://1.117.108.106:8080")))
 s = create(Saya)
 with s.module_context():
     s.require("mod.gnc")
